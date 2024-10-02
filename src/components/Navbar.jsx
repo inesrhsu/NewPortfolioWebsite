baf63169ee5416feaa36d78405312e31c703c0fe
inesrhh <123432531+inesrhh@users.noreply.github.com>
2024-10-02 17:44:55 -0400

    fixes to home page with name visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,40 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
 //import injectSheet from 'react-jss'
-import './../index.css'
-import './Navbar.css'
+import './../index.css';
+import './Navbar.css';
 
 const Navbar = () => {
+
+  const location = useLocation();
+  const [showName, setShowName] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= window.innerHeight/2){
+        setShowName(true);
+      } else {
+        setShowName(false);
+      }
+    };
+
+    if (location.pathname === "/home") {
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [location.pathname]); //run useEffect when location pathname changes
+
   return (
     <header>
+      { showName && (
         <NavLink to="/home" className="icon">
-            <p>INÉS RODRÍGUEZ HSU</p>
-        </NavLink>
+          <p>INÉS RODRÍGUEZ HSU</p>
+        </NavLink> 
+      )}
+        
         <nav className="navbar">
           <NavLink to="/home" className="item">
             <p>Home</p>
