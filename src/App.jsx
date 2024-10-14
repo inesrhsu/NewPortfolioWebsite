97a77ffbcc268f497e7bad9840009e6addeba484
inesrhh <123432531+inesrhh@users.noreply.github.com>
2024-10-14 15:54:21 -0700

    testing small routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { BrowserRouter as Route,Router, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import {Home, About, Work, Contact} from './pages';
